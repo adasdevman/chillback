a666d3fbdaf6c5df90a2b2dab6625ef9cbe0d54a
Franck Adas <103577239+adasdevman@users.noreply.github.com>
2025-01-31 22:38:10 +0000

    userset1

--- chillnowback/settings.py
+++ chillnowback/settings.py
@@ -86,13 +86,13 @@ DATABASES = {
 
 #DATABASES = {
  #   'default': {
- #       'ENGINE': 'django.db.backends.postgresql',
- #       'NAME': os.getenv('DB_NAME'),
- #       'USER': os.getenv('DB_USER'),
- #       'PASSWORD': os.getenv('DB_PASSWORD'),
- #       'HOST': os.getenv('DB_HOST'),
- #       'PORT': os.getenv('DB_PORT'),
-  #  }
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': os.getenv('DB_NAME'),
+    #    'USER': os.getenv('DB_USER'),
+    #    'PASSWORD': os.getenv('DB_PASSWORD'),
+    #    'HOST': os.getenv('DB_HOST'),
+    #    'PORT': os.getenv('DB_PORT'),
+    #}
 #}
 
 AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -2,21 +2,23 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.text import slugify
 
-class CustomUserManager(BaseUserManager):
+class UserManager(BaseUserManager):
     def _create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('L\'email est obligatoire')
         email = self.normalize_email(email)
+        
+        # Générer un username à partir de l'email
         username = slugify(email.split('@')[0])
-        
-        # Assurer un username unique
+        base_username = username
         counter = 1
-        temp_username = username
-        while self.model.objects.filter(username=temp_username).exists():
-            temp_username = f"{username}{counter}"
-            counter += 1
-        username = temp_username
         
+        # S'assurer que le username est unique
+        while self.model.objects.filter(username=username).exists():
+            username = f"{base_username}{counter}"
+            counter += 1
+            
+        # Créer l'utilisateur avec le username généré
         user = self.model(email=email, username=username, **extra_fields)
         if password:
             user.set_password(password)
@@ -41,48 +43,25 @@ class CustomUserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
-    ROLE_CHOICES = [
+    ROLE_CHOICES = (
         ('ADMIN', 'Administrateur'),
         ('ANNONCEUR', 'Annonceur'),
-        ('UTILISATEUR', 'Utilisateur'),
-    ]
+        ('UTILISATEUR', 'Utilisateur')
+    )
 
+    username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='UTILISATEUR')
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    banner_image = models.ImageField(upload_to='banner_images/', null=True, blank=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
-    role = models.CharField(
-        max_length=20,
-        choices=ROLE_CHOICES,
-        default='UTILISATEUR'
-    )
-    profile_image = models.ImageField(
-        upload_to='profile_images/',
-        null=True,
-        blank=True
-    )
-    banner_image = models.ImageField(
-        upload_to='banner_images/',
-        null=True,
-        blank=True
-    )
 
-    objects = CustomUserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def save(self, *args, **kwargs):
-        if not self.username:
-            # Créer un username basé sur l'email
-            base_username = self.email.split('@')[0]
-            username = slugify(base_username)
-            counter = 1
-            while User.objects.filter(username=username).exists():
-                username = f"{slugify(base_username)}{counter}"
-                counter += 1
-            self.username = username
-        super().save(*args, **kwargs)
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
